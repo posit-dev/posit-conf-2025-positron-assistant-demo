991af7ead4ba44f5188b69700584a6c24e166e78
SamEdwardes <sam.edwardes@posit.co>
2025-08-11 14:25:36 -0700

    Added started code for Python/polars/plot9 example with aquarium data

--- python-polars-demo.py
+++ python-polars-demo.py
@@ -1,25 +1,84 @@
+# %% Load libraries and data
+import math
+
+import plotnine as p9
 import polars as pl
-from plotnine import ggplot, aes, geom_tile, labs, theme_minimal, scale_fill_gradient
 
-# 👀 CSV file path -- update this path as needed!
-csv_file_path = 'data/data.csv'
+species = pl.read_csv("./data/georgia-aquarium-species.csv")
+species
 
-# Load CSV file into a Polars DataFrame
-data = pl.read_csv(csv_file_path)
-
-###########################################################################
-# TBD the plotting code probably needs to be adjusted depending on the data
-############################################################################
-
-# Reshape the data from wide to long format using unpivot
-data_long = (
-    data.with_row_index("row")  # Add row indices
-    .unpivot(index="row", variable_name="column", value_name="value")
+# %% Create an order for the categorical data
+conservation_status_enum = pl.Enum(
+    [
+        # Conservation categories from most to least threatened
+        # Threatened...
+        "Critically Endangered",
+        "Endangered",
+        "Vulnerable",
+        # Least concern...
+        "Near Threatened",
+        "Least Concern",
+        # Data...
+        "Data Deficient",
+        "Not Evaluated",
+    ]
 )
 
-# Create heatmap
-(ggplot(data_long, aes(x='column', y='row', fill='value')) +
-        geom_tile() +
-        scale_fill_gradient(low='blue', high='red') +
-        labs(title='Data Heatmap', x='Columns', y='Rows') +
-        theme_minimal())
+species = (
+    species
+    .with_columns(
+        pl.col("conservation_status").cast(conservation_status_enum)
+    )
+)
+
+species
+
+# %% Summarize the number of species by conservation status
+conservation_status_summary = (
+    species
+    .group_by("conservation_status")
+    .len()
+    .sort("conservation_status", descending=True)
+)
+
+conservation_status_summary
+# %% Plot the summary using Plotnine
+threatened_index = 3.5 # Everything from 3.5 to 0 is considered threatened
+
+(
+    p9.ggplot(conservation_status_summary, p9.aes(x="conservation_status", y="len"))
+    + p9.geom_col()
+    + p9.geom_text(
+        p9.aes(label="len"), 
+        ha="left", 
+        nudge_y=1
+    )
+    + p9.geom_vline(
+        p9.aes(xintercept=threatened_index), 
+        linetype="dashed", 
+        color="red"
+    )
+    + p9.annotate(
+        'rect', 
+        xmin=0.5, 
+        xmax=3.5, 
+        ymin=0, 
+        ymax=math.inf, 
+        fill="red", 
+        alpha=0.1
+    )
+    + p9.annotate(
+        "text",
+        label="Species is Threatened",
+        x=threatened_index - 1.5,
+        y=60,
+        color="red",
+    )
+    + p9.scale_y_continuous(expand=(0, 0, 0, 10))
+    + p9.labs(
+        x="Conservation Status",
+        y="Number of Species",
+        title="Species Conservation Status Summary",
+    )
+    + p9.coord_flip()
+)
